6a3ce5bec670a00f6d5cee92efaedb947b019379
togamid <gaisserto80670@th-nuernberg.de>
2022-10-25 17:15:53 +0200

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Logger.getRootLogger().debug("Hello World!");
+    }
+}
